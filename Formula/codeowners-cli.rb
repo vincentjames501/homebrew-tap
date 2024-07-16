6e548a1f79821ccb976dc84e1d5ec674403cd08d
jreleaserbot <jreleaser@kordamp.org>
2024-07-16 16:15:02 +0000

    codeowners-cli v0.0.3

--- Formula/codeowners-cli.rb
+++ Formula/codeowners-cli.rb
@@ -1,26 +1,28 @@
-class CodeownersCli < Formula
-    desc "codeowners-cli -- Command line tool for identifying the owners for files in a local repository or directory as well as identifying unowned codeowners."
-    homepage "https://github.com/vincentjames501/codeowners-cli"
-    version "0.0.1"
-    license "Apache-2.0"
-  
-    if OS.linux? && Hardware::CPU.intel?
-      url "https://github.com/vincentjames501/codeowners-cli/releases/download/v0.0.1/codeowners-cli-0.0.1-linux-x86_64.zip"
-      sha256 "63e6c49fdd6cd287a2899f4da0ecff74b9193b3d44179240df35affd65145c31"
-    end
+# Generated with JReleaser 1.13.0 at 2024-07-16T16:14:50.206537723Z
 
-    if OS.mac? && Hardware::CPU.arm?
-      url "https://github.com/vincentjames501/codeowners-cli/releases/download/v0.0.1/codeowners-cli-0.0.1-osx-aarch_64.zip"
-      sha256 "a10d0278d42652d5d3dbf3761ceb12c19005d5a0802c754ec881f8bd3a0ce9d8"
-    end
-  
-    def install
-      libexec.install Dir["*"]
-      bin.install_symlink "#{libexec}/bin/codeowners-cli"
-    end
-  
-    test do
-      output = shell_output("#{bin}/codeowners-cli --version")
-      assert_match "0.0.1", output
-    end
+class CodeownersCli < Formula
+  desc "CodeOwners CLI utility"
+  homepage "https://github.com/vincentjames501/codeowners-cli"
+  version "0.0.3"
+  license "Apache-2.0"
+
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/vincentjames501/codeowners-cli/releases/download/v0.0.3/codeowners-cli-0.0.3-linux-x86_64.zip"
+    sha256 "fb36bfe9de535eaba987a43c21ae5d3ae8eed24cca85ed18cda9e445bfb22b2f"
   end
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/vincentjames501/codeowners-cli/releases/download/v0.0.3/codeowners-cli-0.0.3-osx-aarch_64.zip"
+    sha256 "9542b392b0ba6a77c4b9edcc0329dc85d9976e49cad1408756448672651d6521"
+  end
+
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink "#{libexec}/bin/codeowners-cli" => "codeowners-cli"
+  end
+
+  test do
+    output = shell_output("#{bin}/codeowners-cli --version")
+    assert_match "0.0.3", output
+  end
+end
