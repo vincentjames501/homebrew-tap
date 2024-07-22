93738cb4cbe26c0ae43f2a603c1c261db22b4ca6
jreleaserbot <jreleaser@kordamp.org>
2024-07-22 22:16:50 +0000

    codeowners-cli v0.0.10

--- Formula/codeowners-cli.rb
+++ Formula/codeowners-cli.rb
@@ -1,18 +1,18 @@
-# Generated with JReleaser 1.13.0 at 2024-07-22T15:58:25.970837677Z
+# Generated with JReleaser 1.13.0 at 2024-07-22T22:07:40.320262255Z
 
 class CodeownersCli < Formula
   desc "Command line tool for codeowners that helps identify the owners for files in a local repository or directory as well as identifying unowned code owners."
   homepage "https://github.com/vincentjames501/codeowners-cli"
-  version "0.0.9"
+  version "0.0.10"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/vincentjames501/codeowners-cli/releases/download/v0.0.9/codeowners-cli-0.0.9-linux-x86_64.zip"
-    sha256 "ecfaa6dc8294e47b1b33fd1cb2d711e869cdcbbed88586f79c8e6e1e9e9dda67"
+    url "https://github.com/vincentjames501/codeowners-cli/releases/download/v0.0.10/codeowners-cli-0.0.10-linux-x86_64.zip"
+    sha256 "8407e8ed14eecc4dd505a7e6a225640de6527b224a420bfaf312902acb38695b"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/vincentjames501/codeowners-cli/releases/download/v0.0.9/codeowners-cli-0.0.9-osx-aarch_64.zip"
-    sha256 "8a4b4c46ffc23ed6add721cb96ac0f1c51de839a016806d3bda5b67668d1360e"
+    url "https://github.com/vincentjames501/codeowners-cli/releases/download/v0.0.10/codeowners-cli-0.0.10-osx-aarch_64.zip"
+    sha256 "7b975c837482d8f9545834643c1f9e832f842f3aa41789ca928e00a86e6772f7"
   end
 
 
@@ -23,6 +23,6 @@ class CodeownersCli < Formula
 
   test do
     output = shell_output("#{bin}/codeowners-cli --version")
-    assert_match "0.0.9", output
+    assert_match "0.0.10", output
   end
 end
